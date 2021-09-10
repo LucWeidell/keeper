ab0527f39cedda5c8aa94d80ea2fb68fbf41b38d
Luc Weidell <>
2021-09-10 11:25:00 -0600

    first commit

---
+++ keeper.client/src/env.js
@@ -0,0 +1,5 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:5001' : ''
+export const domain = 'dev-4ed7g57f.us.auth0.com'
+export const audience = 'https://keeper.com'
+export const clientId = 'uOMPpLip3HYqgMFpq4k4b0Twta7p0XvA'
