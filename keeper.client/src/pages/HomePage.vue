e86f056c0ee93d8cdef9ac3c17e40075ffaf8e36
Luc Weidell <>
2021-09-10 17:05:48 -0600

    css mansory

--- keeper.client/src/pages/HomePage.vue
+++ keeper.client/src/pages/HomePage.vue
@@ -1,17 +1,29 @@
 <template>
   <div class="HomePage container-fluid">
     <div class="row masonry">
-      <div class="col-md-3 masonry-item">
-        <img class="masonry-item" src="'http://placebeard.it/200x300'" alt="">
+      <div class="masonry-item col" style="background-image: http://placebeard.it/200x300; background-size: auto;">
+        <!-- <img class="masonry-item" src="http://placebeard.it/200x300" style="background-image: http://placebeard.it/200x300; background-size: auto;" alt=""> -->
       </div>
-      <div class="col masonry-item">
-        <img class="masonry-content" src="'http://placebeard.it/200x300'" alt="">
+      <div class="masonry-item">
+        <img class="masonry-content" src="http://placebeard.it/200x300" style="background-image: http://placebeard.it/200x400; background-size: auto;" alt="">
       </div>
-      <div class="col masonry-item">
-        <img class="masonry-content" src="'http://placebeard.it/200x300'" alt="">
+      <div class="masonry-item">
+        <img class="masonry-content" src="http://placebeard.it/200x400" style="background-image: http://placebeard.it/200x500; background-size: auto;" alt="">
       </div>
-      <div class="col masonry-item">
-        <img class="masonry-content" src="'http://placebeard.it/200x300'" alt="">
+      <div class="masonry-item">
+        <img class="masonry-content" src="http://placebeard.it/200x500" style="background-image: http://placebeard.it/200x100; background-size: auto;" alt="">
+      </div>
+      <div class="masonry-item">
+        <!-- <img class="masonry-content" src="http://placebeard.it/400x600" alt=""> -->
+      </div>
+      <div class="masonry-item">
+        <!-- <img class="masonry-content" src="http://placebeard.it/300x500" alt=""> -->
+      </div>
+      <div class="masonry-item">
+        <!-- <img class="masonry-content" src="http://placebeard.it/200x300" alt=""> -->
+      </div>
+      <div class="masonry-item">
+        <!-- <img class="masonry-content" src="http://placebeard.it/200x300" alt=""> -->
       </div>
       <KeepCard v-for="k in keeps" :key="k.id" :keep="k" />
     </div>
@@ -19,11 +31,20 @@
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
+// import waitForImages from '../utils/Masonry'
+import { computed, onMounted } from '@vue/runtime-core'
 import { AppState } from '../AppState'
+import Pop from '../utils/Notifier'
 export default {
   name: 'HomePage',
   setup() {
+    onMounted(async() => {
+      try {
+        waitForImages()
+      } catch (error) {
+        Pop.toast(error, 'error')
+      }
+    })
     return {
       keeps: computed(() => AppState.keeps)
     }
@@ -38,8 +59,10 @@ export default {
   .masonry {
     display: grid;
     grid-gap: 10px;
+    // grid-template-columns: min-content min-content;
     grid-template-columns: repeat(auto-fill, minmax(200px,1fr));
-    grid-auto-rows: 0;
+    // grid-auto-rows: auto;
+    grid-auto-rows: minmax(30px, auto);
   }
 
   .masonry-item {
@@ -49,12 +72,14 @@ export default {
   .masonry-item {
      background-color: #eee;
      border-radius: 5px;
-     overflow: hidden;
+     overflow: clip;
   }
 
   .masonry-item,
   .masonry-item img {
      position: relative;
+    //  height: 100%;
+
   }
 
   .masonry-item:after {
@@ -72,5 +97,9 @@ export default {
     justify-content: center;
     align-items: center;
     transition: all .1s ease-in;
+  }
+    .masonry-item:hover:after {
+    font-size: 30px;
+    background-color: rgba(0, 0, 0, .75);
   }
 </style>
